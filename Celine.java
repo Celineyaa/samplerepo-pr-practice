1cd34437f5ccaa5267dbfb8e868ffa8cfb17c645
LIN Siyan <slinbh@connect.ust.hk>
2024-03-06 01:50:14 +0800

    Add a class Celine

---
+++ Celine.java
@@ -0,0 +1,12 @@
+public class Celine {
+    private String duck;
+
+    public Celine() {
+        this.duck = "duckBaby";
+    }
+
+    public static void main(String[] args) {
+        Celine duck = new Celine();
+        System.out.println(duck.duck);
+    }
+}
